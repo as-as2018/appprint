defa760f17e7edf1980c2383b43368439a016f34
as-as2018 <vksriwash002@gmail.com>
2023-11-04 16:22:34 +0530

    a

--- src/App.js
+++ src/App.js
@@ -3,20 +3,53 @@ import React from "react";
 const PrintableContent = ({ headerContent, footerContent, content }) => {
   const [currentPage, setCurrentPage] = React.useState(1);
 
-  const handlePrint = () => {
-    window.print();
-  };
+  const renderData = (content) => {
 
-  const renderContent = () => {
     const pageContent = content.slice(
       (currentPage - 1) * window.innerHeight,
       currentPage * window.innerHeight
     );
 
     return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>City</th>
+            <th>Phone</th>
+            <th>Country</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pageContent.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.city}</td>
+              <td>{item.phone}</td>
+              <td>{item.country}</td>
+              <td>{item.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+
+
+  };
+  const handlePrint = () => {
+    window.print();
+  };
+
+  const renderContent = () => {
+
+
+    return (
       <div>
         <div className="header">{headerContent}</div>
-        <div className="content">{pageContent}</div>
+        <div className="content">{renderData(content)}</div>
         <div className="footer">{footerContent}</div>
       </div>
     );
@@ -43,38 +76,72 @@ const PrintableContent = ({ headerContent, footerContent, content }) => {
 
 // export default PrintableContent;
 
+const renderHeader = (headerData) => {
+  return (<h1>{headerData}</h1>);
 
-const renderTableData = (data) => {
+  // return (
+  //   <table>
+  //     <thead>
+  //       <tr>
+  //         <th>Name</th>
+  //         <th>Email</th>
+  //         <th>City</th>
+  //         <th>Phone</th>
+  //         <th>Country</th>
+  //         <th>Address</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //       {data.map((item) => (
+  //         <tr key={item.id}>
+  //           <td>{item.name}</td>
+  //           <td>{item.email}</td>
+  //           <td>{item.city}</td>
+  //           <td>{item.phone}</td>
+  //           <td>{item.country}</td>
+  //           <td>{item.address}</td>
+  //         </tr>
+  //       ))}
+  //     </tbody>
+  //   </table>
+  // );
+};
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>City</th>
-          <th>Phone</th>
-          <th>Country</th>
-          <th>Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id}>
-            <td>{item.name}</td>
-            <td>{item.email}</td>
-            <td>{item.city}</td>
-            <td>{item.phone}</td>
-            <td>{item.country}</td>
-            <td>{item.address}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+const renderFooter = (footerData) => {
+
+  return (<h1>{footerData}</h1>);
+  // return (
+  //   <table>
+  //     <thead>
+  //       <tr>
+  //         <th>Name</th>
+  //         <th>Email</th>
+  //         <th>City</th>
+  //         <th>Phone</th>
+  //         <th>Country</th>
+  //         <th>Address</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //       {data.map((item) => (
+  //         <tr key={item.id}>
+  //           <td>{item.name}</td>
+  //           <td>{item.email}</td>
+  //           <td>{item.city}</td>
+  //           <td>{item.phone}</td>
+  //           <td>{item.country}</td>
+  //           <td>{item.address}</td>
+  //         </tr>
+  //       ))}
+  //     </tbody>
+  //   </table>
+  // );
 };
 
 const App = () => {
+  const [headerData, setheaderData] = React.useState('This is the header.');
+  const [FooterData, setFooterData] = React.useState('This is the Footer.');
+
   const [data, setData] = React.useState([
     {
       name: "John Doe",
@@ -92,13 +159,573 @@ const App = () => {
       country: "United States",
       address: "456 Elm Street",
     },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      city: "New York",
+      phone: "+1-555-555-5556",
+      country: "United States",
+      address: "456 Elm Street",
+    },
   ]);
 
   return (
     <PrintableContent
-      headerContent="<h1>This is the header.</h1>"
-      footerContent="<h2>This is the footer.</h2>"
-      content={renderTableData(data)}
+      headerContent={renderHeader(headerData)}
+      footerContent={renderFooter(FooterData)}
+      content={data}
     />
   );
 };
